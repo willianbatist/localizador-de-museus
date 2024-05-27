b6c7c23f6eaecab56ecb20c022982c14e54436ec
Willian Batista <willian.alves.b15@gmail.com>
2024-05-27 19:45:34 -0300

    Update MuseumController.java

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * The Controller MuseumController.
  */
-@RestController
+
 @RequestMapping(value = "/museums")
 public class MuseumController {
 
